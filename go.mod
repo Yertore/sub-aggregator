597618205c860876f54b899629c8abcc4a654578
Yertore Zhumatayev <e.jumataev@gmail.com>
2026-06-23 17:24:42 +0500

    init go module, add down migration and env example

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yertore/sub-aggregator
+
+go 1.25.3
---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_subscriptions_user_service_date;
+DROP TABLE IF EXISTS subscriptions;
+DROP EXTENSION IF EXISTS "uuid-ossp";
