37a22b8eac6084dc1e6264326595d927b0337d54
Yertore Zhumatayev <e.jumataev@gmail.com>
2026-06-24 11:29:53 +0500

    feat: connect to postgresql database

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	dsn := "host=localhost port=5435 user=app_user password=app_password dbname=sub_db sslmode=disable"
+
+	db, err := pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		log.Fatal("failed to connect to database:", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(context.Background()); err != nil {
+		log.Fatal("database ping failed:", err)
+	}
+
+	log.Println("connected to database")
+
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "pong")
 	})
