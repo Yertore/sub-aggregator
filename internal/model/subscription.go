12a2c38c33fdcfea372518e87905c62b4743bf37
Yertore Zhumatayev <e.jumataev@gmail.com>
2026-06-24 11:40:35 +0500

    feat: add subscription model

---
+++ internal/model/subscription.go
@@ -0,0 +1,31 @@
+package model
+
+import (
+	"time"
+)
+
+type Subscription struct {
+	ID          string     `json:"id"`
+	ServiceName string     `json:"service_name"`
+	Price       int        `json:"price"`
+	UserID      string     `json:"user_id"`
+	StartDate   time.Time  `json:"start_date"`
+	EndDate     *time.Time `json:"end_date,omitempty"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+}
+
+type CreateSubscriptionRequest struct {
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date,omitempty"`
+}
+
+type UpdateSubscriptionRequest struct {
+	ServiceName string `json:"service_name,omitempty"`
+	Price       int    `json:"price,omitempty"`
+	StartDate   string `json:"start_date,omitempty"`
+	EndDate     string `json:"end_date,omitempty"`
+}
